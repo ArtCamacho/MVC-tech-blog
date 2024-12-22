4c43b602b3e140a52a4735dbd9e66328c3899412
Art Camacho <artcamacho55@gmail.com>
2024-12-22 15:17:36 -0800

    connect to postgresql

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MVC-tech-blog
+## Overview
+The TECH BLOG is an application designed to create a user so this user can make posts and comment on other user's posts. The new user creates an account and can creates posts in their profile visible in the homepage. In the post other users and/or the post creator can add comments.
+
+## Features
+Account Creation - The user creates an account.
+Posts and Comments - Users can create a post and comment on other user's posts.
+Logout - The system logs you out after a certain period of inactivity
+
+## Technologies Used
+1. Javacript
+2. CSS
+3. SQL
+4. Handlebars
+5. Render
+
---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+// Boiler plate stuff
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+// Stuff for blog comments
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id'
+});
+// User has a comment
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog, Comment };
